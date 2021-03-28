4e85cf0264ed63612ed28e8d9bba4f3529c361d7
ammartahir24 <ammartahir1524@gmail.com>
2021-03-28 12:53:36 -0500

    fixed a bug

--- models/client_comm.py
+++ models/client_comm.py
@@ -36,39 +36,39 @@ class ClientComm:
         # To Do: spawn process in mahimahi shell
         client_process = Popen("python3 run_client.py "+ip+" "+str(port), shell=True)
         
-        def train(self, num_epochs, batch_size, minibatch, round_num):
-            # send self.model and params to client
-            soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            soc.connect((self.ip, self.port))
-            soc.send("trn".encode('utf-8'))
-            data = {}
-            data["model"] = self.model
-            data["num_epochs"] = num_epochs
-            data["batch_size"] = batch_size
-            data["minibatch"] = minibatch
-            data["round_num"] = round_num
-            soc.send(json.dumps(jsonpickle.encode(data)).encode('utf-8'))
-        
-        def test(self, set_to_use, round_num):
-            # send self.model and set_to_use to client
-            soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            soc.connect((self.ip, self.port))
-            soc.send("tst".encode('utf-8'))
-            data = {}
-            data["model"] = self.model
-            data["set_to_use"] = set_to_use
-            data["round_num"] = round_num
-            soc.send(json.dumps(jsonpickle.encode(data)).encode('utf-8'))
+    def train(self, num_epochs, batch_size, minibatch, round_num):
+        # send self.model and params to client
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        soc.connect((self.ip, self.port))
+        soc.send("trn".encode('utf-8'))
+        data = {}
+        data["model"] = self.model
+        data["num_epochs"] = num_epochs
+        data["batch_size"] = batch_size
+        data["minibatch"] = minibatch
+        data["round_num"] = round_num
+        soc.send(json.dumps(jsonpickle.encode(data)).encode('utf-8'))
+    
+    def test(self, set_to_use, round_num):
+        # send self.model and set_to_use to client
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        soc.connect((self.ip, self.port))
+        soc.send("tst".encode('utf-8'))
+        data = {}
+        data["model"] = self.model
+        data["set_to_use"] = set_to_use
+        data["round_num"] = round_num
+        soc.send(json.dumps(jsonpickle.encode(data)).encode('utf-8'))
 
-        def stop(self):
-            soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            soc.connect((self.ip, self.port))
-            soc.send("stp".encode('utf-8'))
-        
-        @property
-        def num_samples(self):
-            # get num_samples from client and return
-            soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            soc.connect((self.ip, self.port))
-            soc.send("spl".encode("utf-8"))
-            return int(soc.recv(1023).decode('utf-8'))
+    def stop(self):
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        soc.connect((self.ip, self.port))
+        soc.send("stp".encode('utf-8'))
+    
+    @property
+    def num_samples(self):
+        # get num_samples from client and return
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        soc.connect((self.ip, self.port))
+        soc.send("spl".encode("utf-8"))
+        return int(soc.recv(1023).decode('utf-8'))
